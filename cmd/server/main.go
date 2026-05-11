2e769228b33a0a2a0ee82531b45531e14e28a6f5
mprachi301 <prachim61301@gmail.com>
2026-05-11 17:48:13 +0530

    Implement job service and API handler with routing for job creation and retrieval

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"github.com/mprachi301/payment-retry/config"
+	"github.com/mprachi301/payment-retry/internal/api"
+	"github.com/mprachi301/payment-retry/internal/repository"
+	"github.com/mprachi301/payment-retry/internal/service"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -19,4 +23,16 @@ func main() {
 
 	log.Println("Database connected", db != nil)
 
+	repo := repository.NewJobRepository(db)
+	jobService := service.NewJobService(repo, cnf.MAX_RETRIES)
+	handler := api.NewJobHandler(jobService)
+
+	r := gin.Default()
+	api.SetupRoutes(r, handler)
+
+	log.Println("server starting on port:", cnf.PORT)
+	err = r.Run(":" + cnf.PORT)
+	if err != nil {
+		log.Fatal("failed to start server: ", err)
+	}
 }
